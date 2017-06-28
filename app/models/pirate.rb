891c3c5bea65c14f07c55b972ab3e1793e43f08c
Jexeones24 <jexeones@gmail.com>
2017-06-28 18:22:28 -0400

    Done

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,25 @@
 class Pirate
+  attr_accessor :name, :weight, :height, :ships
+
+  @@all = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    @@all << self
+    @ships = []
+    self.new_ships(args[:ships])
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_ships(args)
+    args.each do |arg|
+      @ships << Ship.new(arg)
+    end
+  end
+
 end
